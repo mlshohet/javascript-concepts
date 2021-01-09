6e63e3540ea484e093f36d8dd1a5c19736812ff5
Mike Shohet <mlshohet@gmail.com>
2021-01-09 16:40:06 -0500

    iife, this and lexical binding

---
+++ iife.js
@@ -0,0 +1,9 @@
+// IIFE - all library code can be placed in local scope
+// to prevent name space collision
+
+// It is immediately invoked with a new environment
+
+(function() {
+	console.log("This will be immidiately invoked");
+})();
+
---
+++ thisTest.js
@@ -0,0 +1,88 @@
+// How to lexically bind objects to functions using 'this' keyword
+
+const a = function() {
+	console.log('a', this);
+	const b = function() {
+		console.log('b', this);
+		const c = {
+			func1: function() {
+				console.log('c', this);
+			}
+		}
+		c.func1();
+	}
+
+	//b gets called by the window/global
+	b();
+}
+
+const obj = {
+	name: 'Kurt',
+	sing() {
+		console.log(this);
+		var func1 = function() {
+			console.log('b', this)
+		}
+		// func1 is called by the wondow/global object:
+		func1();
+		// func1 is not bound lexically to any created object
+	}
+}
+
+// use ES6 arrow functions to lexically bind functions to objects
+
+const obj = {
+	name: 'Kurt',
+	sing() {
+		console.log(this);
+		var func1 = () => {
+			console.log('b', this)
+		}
+		// Now func1 is called by the Object obj:
+		func1();
+		// func1 is bound lexically to obj
+	}
+}
+
+// bind keyword
+
+const obj = {
+	name: 'Kurt',
+	sing() {
+		console.log(this);
+		var func1 = function() {
+			console.log('b', this)
+		}
+		// func1 is bound lexically to 'this':
+		return func1.bind(this);
+	}
+}
+
+// invocation of the bound func1:
+
+obj.sing()();
+
+// Store 'this' object in a reference:
+
+const obj = {
+	name: 'Kurt',
+	sing() {
+		// the calling object is stored
+		var self = this;
+		var func1 = function() {
+			//the calling reference is used in the function
+			console.log('b', self)
+		}
+		
+		return func1;
+	}
+}
+
+
+
+
+
+
+
+
+
