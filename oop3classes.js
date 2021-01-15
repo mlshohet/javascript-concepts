08b2f108fa9eeeb117e140a51d5c5e2ca8652e4e
Mike Shohet <mlshohet@gmail.com>
2021-01-14 19:22:21 -0500

    classes

--- oop3classes.js
+++ oop3classes.js
@@ -18,6 +18,11 @@ class Elf extends Character {
 		super(name, weapon);
 		this.type = type;
 	}
+
+	attack(cry) {
+		console.log(cry);
+		return super.attack();
+	}
 }
 
 class Ogre extends Character {
@@ -34,11 +39,12 @@ class Ogre extends Character {
 // Class instantiation with the keyword 'new'
 const legolas = new Elf('Legolas', 'bow', 'fighter');
 console.log(legolas.name); // prints 'Legolas'
+console.log(legolas.attack("AAAAAAHHHH"));
 
 const elron = new Elf('Elron', 'magic', 'king');
 console.log(elron.name);
-console.log(elron.attack());
-console.log(legolas.attack());
+console.log(elron.attack("Danderos!"));
+console.log(legolas.attack("AHHH!"));
 
 // Extending classes
 const fiona = new Ogre('Fiona', 'pie', 'green');
